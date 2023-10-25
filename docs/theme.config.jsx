765a15cefd27c11dca1212a1237a3147e9a9c2b0
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-10-24 22:57:13 -0700

    docs

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,4 +1,29 @@
 export default {
+  darkMode: false,
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://my-app.com" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || "Anthropic Bedrock"}
+        />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "Anthropic Bedrock are SDKs for Anthropic's models on AWS Bedrock"
+          }
+        />
+      </>
+    );
+  },
   logo: (
     <svg
       width="143"
