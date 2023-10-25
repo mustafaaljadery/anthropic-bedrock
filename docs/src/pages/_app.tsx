d7a4d44bcc6b48d170c70528a42a63f5a81d749b
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-10-24 23:09:45 -0700

    errors

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
